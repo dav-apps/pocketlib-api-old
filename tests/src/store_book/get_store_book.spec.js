3d89b66c2f0d5e0a9d450819d98d328cdfb97158
David <dav2070@web.de>
2020-01-01 19:00:50 +0100

    Add cover to the response of get_store_book and adapt test

--- tests/src/store_book/get_store_book.spec.js
+++ tests/src/store_book/get_store_book.spec.js
@@ -77,5 +77,6 @@ describe("GetStoreBook endpoint", () => {
 		assert.equal(constants.authorUserAuthor.books[0].title, response.data.title);
 		assert.equal(constants.authorUserAuthor.books[0].description, response.data.description);
 		assert.equal(constants.authorUserAuthor.books[0].language, response.data.language);
+		assert.equal(false, response.data.cover);
 	});
 });
