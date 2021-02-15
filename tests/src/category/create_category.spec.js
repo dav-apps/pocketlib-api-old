b07a7d0e11362fdec43202ddc2186bfb3c7d49cb
David <dav2070@web.de>
2021-02-15 22:16:58 +0100

    Update category tests to use access token and new test data
    - Format and clean the code
    - Use new controller functions from dav-npm

--- tests/src/category/create_category.spec.js
+++ tests/src/category/create_category.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai'
 const assert = chai.assert
 import axios from 'axios'
+import { TableObjectsController } from 'dav-npm'
 import constants from '../constants.js'
 import * as utils from '../utils.js'
 
@@ -8,31 +9,34 @@ const createCategoryEndpointUrl = `${constants.apiBaseUrl}/api/1/call/store/cate
 var resetCategories = false
 
 afterEach(async () => {
-	if(resetCategories){
+	if (resetCategories) {
 		await utils.resetCategories()
 		resetCategories = false
 	}
 })
 
 describe("CreateCategory endpoint", () => {
-	it("should not create category without jwt", async () => {
-		try{
+	it("should not create category without access token", async () => {
+		try {
 			await axios.default({
 				method: 'post',
-				url: createCategoryEndpointUrl
+				url: createCategoryEndpointUrl,
+				headers: {
+					'Content-Type': 'application/json'
+				}
 			})
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2101, error.response.data.errors[0].code);
-			return;
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2101, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not create category with invalid jwt", async () => {
-		try{
+	it("should not create category with access token for session that does not exist", async () => {
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
@@ -40,260 +44,253 @@ describe("CreateCategory endpoint", () => {
 					Authorization: "asdasdasad",
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(401, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1302, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(404, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2802, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not create category without content type json", async () => {
-		try{
+	it("should not create category without Content-Type json", async () => {
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt
+					Authorization: constants.davUser.accessToken
 				}
-			});
-		}catch(error){
-			assert.equal(415, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1104, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(415, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1104, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not create category if jwt is for another app", async () => {
-		try{
+	it("should not create category with access token for another app", async () => {
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davClassLibraryTestUserTestAppJWT,
+					Authorization: constants.testUserTestAppAccessToken,
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(403, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1102, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(403, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1102, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category if the user is not an admin", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.authorUser.jwt,
+					Authorization: constants.authorUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name: "Test"
 				}
-			});
-		}catch(error){
-			assert.equal(403, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1102, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(403, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1102, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category without required properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2111, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2111, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category with properties with wrong types", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key: 12
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2213, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2213, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category with too short properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key: "a"
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2310, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2310, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category with too long properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key: "a".repeat(200)
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2410, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2410, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category with invalid key", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key: "hello world"
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2502, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2502, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not create category with key that is already used", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key: constants.categories[0].key
 				}
-			});
-		}catch(error){
-			assert.equal(422, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2601, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(422, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2601, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should create category", async () => {
-		let response;
-		let key = "test";
+		resetCategories = true
+		let response
+		let key = "test"
 
 		// Create the category
-		try{
+		try {
 			response = await axios.default({
 				method: 'post',
 				url: createCategoryEndpointUrl,
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					key
 				}
-			});
-		}catch(error){
-			assert.fail();
+			})
+		} catch (error) {
+			assert.fail()
 		}
 
-		assert.equal(201, response.status);
-		assert(response.data.uuid != null);
-		assert.equal(key, response.data.key);
-		assert.equal(0, response.data.names);
+		assert.equal(201, response.status)
+		assert(response.data.uuid != null)
+		assert.equal(key, response.data.key)
+		assert.equal(0, response.data.names)
 
 		// Check if the data was correctly saved on the server
 		// Get the Category table object
-		let categoryObjResponse;
+		let categoryObjResponse = await TableObjectsController.GetTableObject({
+			accessToken: constants.davUser.accessToken,
+			uuid: response.data.uuid
+		})
 
-		try{
-			categoryObjResponse = await axios.default({
-				method: 'get',
-				url: `${constants.apiBaseUrl}/apps/object/${response.data.uuid}`,
-				headers: {
-					Authorization: constants.davUser.jwt
-				}
-			});
-		}catch(error){
-			assert.fail();
+		if (categoryObjResponse.status != 200) {
+			assert.fail()
 		}
 
-		assert.equal(key, categoryObjResponse.data.properties.key);
-		assert.equal(null, categoryObjResponse.data.properties.names);
-
-		// Tidy up
-		resetCategories = true;
-	});
-});
+		assert.equal(key, categoryObjResponse.data.GetPropertyValue("key"))
+		assert.equal(null, categoryObjResponse.data.GetPropertyValue("names"))
+	})
+})
--- tests/src/category/get_categories.spec.js
+++ tests/src/category/get_categories.spec.js
@@ -7,36 +7,36 @@ const getCategoriesEndpointUrl = `${constants.apiBaseUrl}/api/1/call/store/categ
 
 describe("GetCategories endpoint", async () => {
 	it("should return categories", async () => {
-		let response;
+		let response
 
-		try{
+		try {
 			response = await axios.default({
 				method: 'get',
 				url: getCategoriesEndpointUrl,
 				headers: {
-					Authorization: constants.davClassLibraryTestUser.jwt
+					Authorization: constants.testUser.accessToken
 				}
-			});
-		}catch(error){
-			assert.fail();
+			})
+		} catch (error) {
+			assert.fail()
 		}
 
-		assert.equal(200, response.status);
+		assert.equal(200, response.status)
 
-		let i = 0;
-		for(let category of constants.categories){
-			assert.equal(category.uuid, response.data.categories[i].uuid);
-			assert.equal(category.key, response.data.categories[i].key);
+		let i = 0
+		for (let category of constants.categories) {
+			assert.equal(category.uuid, response.data.categories[i].uuid)
+			assert.equal(category.key, response.data.categories[i].key)
 
-			let j = 0;
-			for(let categoryName of category.names){
-				assert.equal(categoryName.name, response.data.categories[i].names[j].name);
-				assert.equal(categoryName.language, response.data.categories[i].names[j].language);
+			let j = 0
+			for (let categoryName of category.names) {
+				assert.equal(categoryName.name, response.data.categories[i].names[j].name)
+				assert.equal(categoryName.language, response.data.categories[i].names[j].language)
 
-				j++;
+				j++
 			}
 
-			i++;
+			i++
 		}
-	});
-});
+	})
+})
