25b04fda7603887acec4cec9cdbb6c6686f0d9e7
David <dav2070@web.de>
2021-02-15 22:59:50 +0100

    Update set_category_name tests to use access token and new test data
    - Format and clean the code
    - Use new controller functions from dav-npm

--- tests/src/category_name/set_category_name.spec.js
+++ tests/src/category_name/set_category_name.spec.js
@@ -1,6 +1,7 @@
 import chai from 'chai'
 const assert = chai.assert
 import axios from 'axios'
+import { TableObjectsController } from 'dav-npm'
 import constants from '../constants.js'
 import * as utils from '../utils.js'
 
@@ -8,7 +9,7 @@ const setCategoryNameEndpointUrl = `${constants.apiBaseUrl}/api/1/call/store/cat
 var resetCategoriesAndCategoryNames = false
 
 afterEach(async () => {
-	if(resetCategoriesAndCategoryNames){
+	if (resetCategoriesAndCategoryNames) {
 		await utils.resetCategories()
 		await utils.resetCategoryNames()
 		resetCategoriesAndCategoryNames = false
@@ -16,24 +17,27 @@ afterEach(async () => {
 })
 
 describe("SetCategoryName endpoint", () => {
-	it("should not set category name without jwt", async () => {
-		try{
+	it("should not set category name without access token", async () => {
+		try {
 			await axios.default({
 				method: 'put',
-				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en")
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2101, error.response.data.errors[0].code);
-			return;
+				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2101, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not set category name with invalid jwt", async () => {
-		try{
+	it("should not set category name with access token for session that does not exist", async () => {
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
@@ -41,343 +45,322 @@ describe("SetCategoryName endpoint", () => {
 					Authorization: "asdasdasdas",
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(401, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1302, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(404, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2802, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not set category name without content type json", async () => {
-		try{
+	it("should not set category name without Content-Type json", async () => {
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/xml'
 				}
-			});
-		}catch(error){
-			assert.equal(415, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1104, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(415, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1104, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
-	it("should not set category name if jwt is for another app", async () => {
-		try{
+	it("should not set category name with access token for another app", async () => {
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davClassLibraryTestUserTestAppJWT,
+					Authorization: constants.testUserTestAppAccessToken,
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(403, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1102, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(403, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1102, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name if the user is not an admin", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.authorUser.jwt,
+					Authorization: constants.authorUser.accessToken,
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(403, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1102, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(403, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1102, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name without required properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2108, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2108, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name with properties with wrong types", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name: false
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2209, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2209, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name with too short properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name: "a"
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2307, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2307, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name with too long properties", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "en"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name: "a".repeat(150)
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(2407, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(2407, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should not set category name for not supported language", async () => {
-		try{
+		try {
 			await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', constants.categories[0].uuid).replace('{1}', "bla"),
 				headers: {
-					Authorization: constants.davUser.jwt,
+					Authorization: constants.davUser.accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name: "Hello World"
 				}
-			});
-		}catch(error){
-			assert.equal(400, error.response.status);
-			assert.equal(1, error.response.data.errors.length);
-			assert.equal(1107, error.response.data.errors[0].code);
-			return;
+			})
+		} catch (error) {
+			assert.equal(400, error.response.status)
+			assert.equal(1, error.response.data.errors.length)
+			assert.equal(1107, error.response.data.errors[0].code)
+			return
 		}
 
-		assert.fail();
-	});
+		assert.fail()
+	})
 
 	it("should create category name", async () => {
-		// Create the category name
-		let response;
-		let category = constants.categories[1];
-		let language = "fr";
-		let name = "Hello World!";
-		let jwt = constants.davUser.jwt;
+		resetCategoriesAndCategoryNames = true
 
-		try{
+		// Create the category name
+		let response
+		let category = constants.categories[1]
+		let language = "fr"
+		let name = "Hello World!"
+		let accessToken = constants.davUser.accessToken
+
+		try {
 			response = await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', category.uuid).replace('{1}', language),
 				headers: {
-					Authorization: jwt,
+					Authorization: accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name
 				}
-			});
-		}catch(error){
-			assert.fail();
+			})
+		} catch (error) {
+			assert.fail()
 		}
 
-		assert.equal(200, response.status);
-		assert.equal(name, response.data.name);
-		assert.equal(language, response.data.language);
+		assert.equal(200, response.status)
+		assert.equal(name, response.data.name)
+		assert.equal(language, response.data.language)
 
 		// Check if the data was correctly saved on the server
 		// Get the category table object
-		let categoryObjResponse;
+		let categoryObjResponse = await TableObjectsController.GetTableObject({
+			accessToken,
+			uuid: category.uuid
+		})
 
-		try{
-			categoryObjResponse = await axios.default({
-				method: 'get',
-				url: `${constants.apiBaseUrl}/apps/object/${category.uuid}`,
-				headers: {
-					Authorization: jwt
-				}
-			});
-		}catch(error){
-			assert.fail();
+		if (categoryObjResponse.status != 200) {
+			assert.fail()
 		}
 
-		let responseCategoryNames = categoryObjResponse.data.properties.names;
-		let responseCategoryNameUuids = responseCategoryNames.split(',');
+		let responseCategoryNames = categoryObjResponse.data.GetPropertyValue("names")
+		let responseCategoryNameUuids = responseCategoryNames.split(',')
 
-		let categoryNameUuids = [];
-		category.names.forEach(name => categoryNameUuids.push(name.uuid));
-		categoryNameUuids.push(responseCategoryNameUuids[responseCategoryNameUuids.length - 1]);
-		let categoryNames = categoryNameUuids.join(',');
+		let categoryNameUuids = []
+		category.names.forEach(name => categoryNameUuids.push(name.uuid))
+		categoryNameUuids.push(responseCategoryNameUuids[responseCategoryNameUuids.length - 1])
+		let categoryNames = categoryNameUuids.join(',')
 
-		assert.equal(categoryNameUuids.length, responseCategoryNameUuids.length);
-		assert.equal(categoryNames, responseCategoryNames);
+		assert.equal(categoryNameUuids.length, responseCategoryNameUuids.length)
+		assert.equal(categoryNames, responseCategoryNames)
 
 		// Get the category name table object
-		let categoryNameObjResponse;
-		let newCategoryNameUuid = responseCategoryNameUuids[responseCategoryNameUuids.length - 1];
+		let newCategoryNameUuid = responseCategoryNameUuids[responseCategoryNameUuids.length - 1]
 
-		try{
-			categoryNameObjResponse = await axios.default({
-				method: 'get',
-				url: `${constants.apiBaseUrl}/apps/object/${newCategoryNameUuid}`,
-				headers: {
-					Authorization: jwt
-				}
-			});
-		}catch(error){
-			assert.fail();
+		let categoryNameObjResponse = await TableObjectsController.GetTableObject({
+			accessToken,
+			uuid: newCategoryNameUuid
+		})
+
+		if (categoryNameObjResponse.status != 200) {
+			assert.fail()
 		}
 
-		assert.equal(newCategoryNameUuid, categoryNameObjResponse.data.uuid);
-		assert.equal(name, categoryNameObjResponse.data.properties.name);
-		assert.equal(language, categoryNameObjResponse.data.properties.language);
-
-		// Tidy up
-		resetCategoriesAndCategoryNames = true;
-	});
+		assert.equal(newCategoryNameUuid, categoryNameObjResponse.data.Uuid)
+		assert.equal(name, categoryNameObjResponse.data.GetPropertyValue("name"))
+		assert.equal(language, categoryNameObjResponse.data.GetPropertyValue("language"))
+	})
 
 	it("should update category name", async () => {
-		// Update the category name
-		let response;
-		let category = constants.categories[1];
-		let language = "en";
-		let name = "Updated name";
-		let categoryNameUuid = category.names[0].uuid;
-		let jwt = constants.davUser.jwt;
+		resetCategoriesAndCategoryNames = true
 
-		try{
+		// Update the category name
+		let response
+		let category = constants.categories[1]
+		let language = "en"
+		let name = "Updated name"
+		let categoryNameUuid = category.names[0].uuid
+		let accessToken = constants.davUser.accessToken
+
+		try {
 			response = await axios.default({
 				method: 'put',
 				url: setCategoryNameEndpointUrl.replace('{0}', category.uuid).replace('{1}', language),
 				headers: {
-					Authorization: jwt,
+					Authorization: accessToken,
 					'Content-Type': 'application/json'
 				},
 				data: {
 					name
 				}
-			});
-		}catch(error){
-			assert.fail();
+			})
+		} catch (error) {
+			assert.fail()
 		}
 
-		assert.equal(200, response.status);
-		assert.equal(name, response.data.name);
-		assert.equal(language, response.data.language);
+		assert.equal(200, response.status)
+		assert.equal(name, response.data.name)
+		assert.equal(language, response.data.language)
 
 		// Check if the data was correctly updated on the server
 		// Get the category
-		let categoryObjResponse;
+		let categoryObjResponse = await TableObjectsController.GetTableObject({
+			accessToken,
+			uuid: category.uuid
+		})
 
-		try{
-			categoryObjResponse = await axios.default({
-				method: 'get',
-				url: `${constants.apiBaseUrl}/apps/object/${category.uuid}`,
-				headers: {
-					Authorization: jwt
-				}
-			});
-		}catch(error){
-			assert.fail();
+		if (categoryObjResponse.status != 200) {
+			assert.fail()
 		}
 
-		let responseCategoryNames = categoryObjResponse.data.properties.names;
-		let responseCategoryNameUuids = responseCategoryNames.split(',');
+		let responseCategoryNames = categoryObjResponse.data.GetPropertyValue("names")
+		let responseCategoryNameUuids = responseCategoryNames.split(',')
 
-		let categoryNameUuids = [];
-		category.names.forEach(name => categoryNameUuids.push(name.uuid));
-		let categoryNames = categoryNameUuids.join(',');
+		let categoryNameUuids = []
+		category.names.forEach(name => categoryNameUuids.push(name.uuid))
+		let categoryNames = categoryNameUuids.join(',')
 
-		assert.equal(categoryNameUuids.length, responseCategoryNameUuids.length);
-		assert.equal(categoryNames, responseCategoryNames);
+		assert.equal(categoryNameUuids.length, responseCategoryNameUuids.length)
+		assert.equal(categoryNames, responseCategoryNames)
 
 		// Get the category name table object
-		let categoryNameObjResponse;
+		let categoryNameObjResponse = await TableObjectsController.GetTableObject({
+			accessToken,
+			uuid: categoryNameUuid
+		})
 
-		try{
-			categoryNameObjResponse = await axios.default({
-				method: 'get',
-				url: `${constants.apiBaseUrl}/apps/object/${categoryNameUuid}`,
-				headers: {
-					Authorization: jwt
-				}
-			});
-		}catch(error){
-			assert.fail();
+		if (categoryNameObjResponse.status != 200) {
+			assert.fail()
 		}
 
-		assert.equal(categoryNameUuid, categoryNameObjResponse.data.uuid);
-		assert.equal(name, categoryNameObjResponse.data.properties.name);
-		assert.equal(language, categoryNameObjResponse.data.properties.language);
-
-		// Tidy up
-		resetCategoriesAndCategoryNames = true;
-	});
-});
+		assert.equal(categoryNameUuid, categoryNameObjResponse.data.Uuid)
+		assert.equal(name, categoryNameObjResponse.data.GetPropertyValue("name"))
+		assert.equal(language, categoryNameObjResponse.data.GetPropertyValue("language"))
+	})
+})
