0812a14d7f2dfbc7ef1e733f530e453c1a886c1f
David <dav2070@web.de>
2022-03-12 20:52:05 +0100

    Simplify get_author, add ability to select result fields & update tests
    - Add process_fields function to read the fields url param and convert it to a list of param strings
    - Remove the ability to get the books of the user in get_author
    - Update & simplify tests for get_author

--- tests/src/author/get_author.spec.js
+++ tests/src/author/get_author.spec.js
@@ -14,9 +14,9 @@ describe("GetAuthor endpoint", async () => {
 				url: getAuthorEndpointUrl.replace('{0}', "asdasdasd")
 			})
 		} catch (error) {
-			assert.equal(404, error.response.status)
-			assert.equal(1, error.response.data.errors.length)
-			assert.equal(ErrorCodes.AuthorDoesNotExist, error.response.data.errors[0].code)
+			assert.equal(error.response.status, 404)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.AuthorDoesNotExist)
 			return
 		}
 
@@ -30,29 +30,9 @@ describe("GetAuthor endpoint", async () => {
 				url: getAuthorEndpointUrl.replace('{0}', constants.davUser.authors[0].bios[0].uuid)
 			})
 		} catch (error) {
-			assert.equal(403, error.response.status)
-			assert.equal(1, error.response.data.errors.length)
-			assert.equal(ErrorCodes.ActionNotAllowed, error.response.data.errors[0].code)
-			return
-		}
-
-		assert.fail()
-	})
-
-	it("should not return author with books with not supported language", async () => {
-		try {
-			await axios({
-				method: 'get',
-				url: getAuthorEndpointUrl.replace('{0}', constants.authorUser.author.uuid),
-				params: {
-					books: true,
-					languages: "asd"
-				}
-			})
-		} catch (error) {
-			assert.equal(400, error.response.status)
-			assert.equal(1, error.response.data.errors.length)
-			assert.equal(ErrorCodes.LanguageNotSupported, error.response.data.errors[0].code)
+			assert.equal(error.response.status, 403)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.ActionNotAllowed)
 			return
 		}
 
@@ -66,30 +46,6 @@ describe("GetAuthor endpoint", async () => {
 	it("should return author of admin", async () => {
 		await testGetAuthor(constants.davUser.authors[0])
 	})
-
-	it("should return author with books", async () => {
-		await testGetAuthorWithBooks(constants.authorUser.author)
-	})
-
-	it("should return author with books with specified language", async () => {
-		await testGetAuthorWithBooks(constants.authorUser.author, ["de"])
-	})
-
-	it("should return author with books with specified languages", async () => {
-		await testGetAuthorWithBooks(constants.authorUser.author, ["en", "de"])
-	})
-
-	it("should return author of admin with books", async () => {
-		await testGetAuthorWithBooks(constants.davUser.authors[0])
-	})
-
-	it("should return author of admin with books with specified language", async () => {
-		await testGetAuthorWithBooks(constants.davUser.authors[0], ["de"])
-	})
-
-	it("should return author of admin with books with specified languages", async () => {
-		await testGetAuthorWithBooks(constants.davUser.authors[0], ["en", "de"])
-	})
 })
 
 async function testGetAuthor(author) {
@@ -98,121 +54,50 @@ async function testGetAuthor(author) {
 	try {
 		response = await axios({
 			method: 'get',
-			url: getAuthorEndpointUrl.replace('{0}', author.uuid)
+			url: getAuthorEndpointUrl.replace('{0}', author.uuid),
+			params: {
+				fields: "*"
+			}
 		})
 	} catch (error) {
 		assert.fail()
 	}
 
-	assert.equal(200, response.status)
-	assert.equal(author.uuid, response.data.uuid)
-	assert.equal(author.firstName, response.data.first_name)
-	assert.equal(author.lastName, response.data.last_name)
-	assert.equal(author.websiteUrl, response.data.website_url)
-	assert.equal(author.facebookUsername, response.data.facebook_username)
-	assert.equal(author.instagramUsername, response.data.instagram_username)
-	assert.equal(author.twitterUsername, response.data.twitter_username)
-	assert.equal(author.bios.length, response.data.bios.length)
-	assert.equal(author.collections.length, response.data.collections.length)
-	assert.equal(author.profileImage != null, response.data.profile_image)
-	assert.equal(author.profileImageBlurhash, response.data.profile_image_blurhash)
+	assert.equal(response.status, 200)
+	assert.equal(response.data.uuid, author.uuid)
+	assert.equal(response.data.first_name, author.firstName)
+	assert.equal(response.data.last_name, author.lastName)
+	assert.equal(response.data.website_url, author.websiteUrl)
+	assert.equal(response.data.facebook_username, author.facebookUsername)
+	assert.equal(response.data.instagram_username, author.instagramUsername)
+	assert.equal(response.data.twitter_username, author.twitterUsername)
+	assert.equal(response.data.bios.length, author.bios.length)
+	assert.equal(response.data.collections.length, author.collections.length)
+	assert.equal(response.data.profile_image, author.profileImage != null)
+	assert.equal(response.data.profile_image_blurhash, author.profileImageBlurhash)
 
 	for (let i = 0; i < author.bios.length; i++) {
 		let bio = author.bios[i]
 		let responseBio = response.data.bios[i]
 
 		assert.isUndefined(responseBio.uuid)
-		assert.equal(bio.bio, responseBio.bio)
-		assert.equal(bio.language, responseBio.language)
+		assert.equal(responseBio.bio, bio.bio)
+		assert.equal(responseBio.language, bio.language)
 	}
 
 	for (let i = 0; i < author.collections.length; i++) {
 		let collection = author.collections[i]
 		let responseCollection = response.data.collections[i]
 
-		assert.equal(collection.uuid, responseCollection.uuid)
+		assert.equal(responseCollection.uuid, collection.uuid)
 
 		for (let j = 0; j < collection.names.length; j++) {
 			let name = collection.names[j]
 			let responseName = responseCollection.names[j]
 
 			assert.isUndefined(responseName.uuid)
-			assert.equal(name.name, responseName.name)
-			assert.equal(name.language, responseName.language)
+			assert.equal(responseName.name, name.name)
+			assert.equal(responseName.language, name.language)
 		}
-	}
-}
-
-async function testGetAuthorWithBooks(author, languages) {
-	let response
-
-	try {
-		let options = {
-			method: 'get',
-			url: getAuthorEndpointUrl.replace('{0}', author.uuid),
-			params: {
-				books: true
-			}
-		}
-
-		if (languages) {
-			options.params["languages"] = languages.join(',')
-		}
-
-		response = await axios(options)
-	} catch (error) {
-		assert.fail()
-	}
-
-	if (!languages) {
-		languages = ["en"]
-	}
-
-	// Find the store books
-	let storeBooks = []
-	for (let collection of author.collections) {
-		for (let storeBook of collection.books) {
-			if (storeBook.status == "published" && languages.includes(storeBook.language)) {
-				storeBooks.push(storeBook)
-			}
-		}
-	}
-
-	assert.equal(200, response.status)
-	assert.equal(author.uuid, response.data.uuid)
-	assert.equal(author.firstName, response.data.first_name)
-	assert.equal(author.lastName, response.data.last_name)
-	assert.equal(author.websiteUrl, response.data.website_url)
-	assert.equal(author.facebookUsername, response.data.facebook_username)
-	assert.equal(author.instagramUsername, response.data.instagram_username)
-	assert.equal(author.twitterUsername, response.data.twitter_username)
-	assert.equal(author.bios.length, response.data.bios.length)
-	assert.equal(storeBooks.length, response.data.books.length)
-	assert.equal(author.profileImage != null, response.data.profile_image)
-	assert.equal(author.profileImageBlurhash, response.data.profile_image_blurhash)
-
-	for (let i = 0; i < author.bios.length; i++) {
-		let bio = author.bios[i]
-		let responseBio = response.data.bios[i]
-
-		assert.isUndefined(responseBio.uuid)
-		assert.equal(bio.bio, responseBio.bio)
-		assert.equal(bio.language, responseBio.language)
-	}
-
-	for (let book of response.data.books) {
-		let storeBook = storeBooks.find(sBook => sBook.uuid == book.uuid)
-		
-		assert.isNotNull(storeBook)
-		assert.equal(storeBook.uuid, book.uuid)
-		assert.equal(storeBook.title, book.title)
-		assert.equal(storeBook.description, book.description)
-		assert.equal(storeBook.language, book.language)
-		assert.equal(storeBook.status, book.status)
-		assert.equal(storeBook.cover != null, book.cover)
-		assert.equal(storeBook.coverAspectRatio, book.cover_aspect_ratio)
-		assert.equal(storeBook.coverBlurhash, book.cover_blurhash)
-		assert.equal(storeBook.file != null, book.file)
-		assert.equal(storeBook.fileName, book.file_name)
 	}
 }
