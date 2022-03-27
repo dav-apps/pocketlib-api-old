c847cbfdbd1733eeb02771ae1ca5a2bc7a2d9567
David <dav2070@web.de>
2022-03-27 20:29:14 +0200

    Add bio to response of get_latest_authors and update tests
    - Add the ability to specify the preferred languages for the bios

--- tests/src/author/get_latest_authors.spec.js
+++ tests/src/author/get_latest_authors.spec.js
@@ -31,22 +31,92 @@ describe("GetLatestAuthors endpoint", () => {
 		assert.equal(response.status, 200)
 		assert.equal(response.data.authors.length, authors.length)
 
-		let i = 0
-		for (let author of response.data.authors) {
-			assert.equal(author.uuid, authors[i].uuid)
-			assert.equal(author.first_name, authors[i].firstName)
-			assert.equal(author.last_name, authors[i].lastName)
-			assert.equal(author.website_url, authors[i].websiteUrl)
-			assert.equal(author.facebook_username, authors[i].facebookUsername)
-			assert.equal(author.instagram_username, authors[i].instagramUsername)
-			assert.equal(author.twitter_username, authors[i].twitterUsername)
-			assert.equal(author.profile_image_blurhash, authors[i].profileImageBlurhash)
+		for (let author of authors) {
+			let responseAuthor = response.data.authors.find(a => a.uuid == author.uuid)
 
-			i++
+			assert.isNotNull(responseAuthor)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image_blurhash, author.profileImageBlurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == "en")
+
+				assert.isNotNull(authorBio)
+				assert.equal(responseAuthor.bio.language, "en")
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			}
 		}
 	})
 
-	it("should return latest authors with limit", async () => {
+	it("should return latest authors with specified language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: getLatestAuthorsEndpointUrl,
+				params: {
+					fields: "*",
+					languages: language
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		// Find all authors with a profile image
+		let authors = [constants.authorUser.author]
+		for (let author of constants.davUser.authors) {
+			if (author.profileImage) authors.push(author)
+		}
+		authors = authors.reverse()
+
+		assert.equal(response.status, 200)
+		assert.equal(response.data.authors.length, authors.length)
+
+		for (let author of authors) {
+			let responseAuthor = response.data.authors.find(a => a.uuid == author.uuid)
+
+			assert.isNotNull(responseAuthor)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image_blurhash, author.profileImageBlurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == language)
+
+				if (authorBio == null) {
+					assert.equal(responseAuthor.bio.language, "en")
+
+					authorBio = author.bios.find(b => b.language == "en")
+
+					assert.isNotNull(authorBio)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				} else {
+					assert.equal(responseAuthor.bio.language, language)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				}
+			}
+		}
+	})
+
+	it("should return latest authors with specified limit", async () => {
 		let response
 
 		try {
@@ -82,5 +152,15 @@ describe("GetLatestAuthors endpoint", () => {
 		assert.equal(responseAuthor.instagram_username, author.instagramUsername)
 		assert.equal(responseAuthor.twitter_username, author.twitterUsername)
 		assert.equal(responseAuthor.profile_image_blurhash, author.profileImageBlurhash)
+
+		if (author.bios.length == 0) {
+			assert.isNull(responseAuthor.bio)
+		} else {
+			let authorBio = author.bios.find(b => b.language == "en")
+
+			assert.isNotNull(authorBio)
+			assert.equal(responseAuthor.bio.language, "en")
+			assert.equal(responseAuthor.bio.value, authorBio.bio)
+		}
 	})
 })
