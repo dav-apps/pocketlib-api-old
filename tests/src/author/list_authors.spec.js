2f2551711d038863abbed6001e3ae391f7e9e0cf
David <dav2070@web.de>
2022-03-29 22:28:58 +0200

    Add list_authors endpoint and implement tests
    - Add list_authors endpoint for getting a list of authors, which are either the authors of the current admin user or the latest authors, with the ability to specify the fields to returns and to set the preferred languages and the max number of authors
    - Add get_author_table_object_uuids for getting only the uuids of the authors, so that table objects that are not returned are not initialized in TableObjectHolder

---
+++ tests/src/author/list_authors.spec.js
@@ -0,0 +1,440 @@
+import chai from 'chai'
+const assert = chai.assert
+import axios from 'axios'
+import constants from '../constants.js'
+import * as ErrorCodes from '../errorCodes.js'
+
+const listAuthorsEndpointUrl = `${constants.apiBaseUrl}/authors`
+
+describe("ListAuthors endpoint", () => {
+	it("should not return authors of admin without access token", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				params: {
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 401)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.AuthorizationHeaderMissing)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return authors of admin with access token for session that does not exist", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: "asdasdasdasdasd"
+				},
+				params: {
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 404)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.SessionDoesNotExist)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return authors of admin with access token for another app", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: constants.testUserTestAppAccessToken
+				},
+				params: {
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 403)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.ActionNotAllowed)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return authors of admin if the user is not an admin", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: constants.authorUser.accessToken
+				},
+				params: {
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 400)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.UserIsNotAdmin)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should return authors of admin", async () => {
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: constants.davUser.accessToken
+				},
+				params: {
+					fields: "*",
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, constants.davUser.authors.length)
+
+		for (let author of constants.davUser.authors) {
+			let responseAuthor = response.data.items.find(a => a.uuid == author.uuid)
+
+			assert.isNotNull(responseAuthor)
+			assert.equal(Object.keys(responseAuthor).length, 9)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == "en")
+
+				assert.isNotNull(authorBio)
+				assert.equal(responseAuthor.bio.language, "en")
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			}
+		}
+	})
+
+	it("should return authors of admin with specified language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: constants.davUser.accessToken
+				},
+				params: {
+					fields: "*",
+					languages: language,
+					mine: true
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, constants.davUser.authors.length)
+
+		for (let author of constants.davUser.authors) {
+			let responseAuthor = response.data.items.find(a => a.uuid == author.uuid)
+
+			assert.isNotNull(responseAuthor)
+			assert.equal(Object.keys(responseAuthor).length, 9)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == language)
+
+				if (authorBio == null) {
+					assert.equal(responseAuthor.bio.language, "en")
+
+					authorBio = author.bios.find(b => b.language == "en")
+
+					assert.isNotNull(authorBio)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				} else {
+					assert.equal(responseAuthor.bio.language, language)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				}
+			}
+		}
+	})
+
+	it("should return authors of admin with specified limit and language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				headers: {
+					Authorization: constants.davUser.accessToken
+				},
+				params: {
+					fields: "*",
+					languages: language,
+					limit: 1,
+					mine: true,
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+	
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, 1)
+
+		let responseAuthor = response.data.items[0]
+		let author = constants.davUser.authors.find(a => a.uuid == responseAuthor.uuid)
+
+		assert.isNotNull(author)
+		assert.equal(Object.keys(responseAuthor).length, 9)
+		assert.equal(responseAuthor.uuid, author.uuid)
+		assert.equal(responseAuthor.first_name, author.firstName)
+		assert.equal(responseAuthor.last_name, author.lastName)
+		assert.equal(responseAuthor.website_url, author.websiteUrl)
+		assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+		assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+		assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+		assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+		if (author.bios.length == 0) {
+			assert.isNull(responseAuthor.bio)
+		} else {
+			let authorBio = author.bios.find(b => b.language == language)
+
+			if (authorBio == null) {
+				assert.equal(responseAuthor.bio.language, "en")
+
+				authorBio = author.bios.find(b => b.language == "en")
+
+				assert.isNotNull(authorBio)
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			} else {
+				assert.equal(responseAuthor.bio.language, language)
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			}
+		}
+	})
+
+	it("should return latest authors", async () => {
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				params: {
+					fields: "*",
+					latest: true
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		// Find all authors with a profile image
+		let authors = [constants.authorUser.author]
+		for (let author of constants.davUser.authors) {
+			if (author.profileImageItem) authors.push(author)
+		}
+		authors = authors.reverse()
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, authors.length)
+
+		for (let author of authors) {
+			let responseAuthor = response.data.items.find(a => a.uuid == author.uuid)
+
+			assert.isNotNull(responseAuthor)
+			assert.equal(Object.keys(responseAuthor).length, 9)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == "en")
+
+				assert.isNotNull(authorBio)
+				assert.equal(responseAuthor.bio.language, "en")
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			}
+		}
+	})
+
+	it("should return latest authors with specified language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				params: {
+					fields: "*",
+					languages: language,
+					latest: true
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		// Find all authors with a profile image
+		let authors = [constants.authorUser.author]
+		for (let author of constants.davUser.authors) {
+			if (author.profileImageItem) authors.push(author)
+		}
+		authors = authors.reverse()
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, authors.length)
+
+		for (let author of authors) {
+			let responseAuthor = response.data.items.find(a => a.uuid == author.uuid)
+
+			assert.isNotNull(responseAuthor)
+			assert.equal(Object.keys(responseAuthor).length, 9)
+			assert.equal(responseAuthor.uuid, author.uuid)
+			assert.equal(responseAuthor.first_name, author.firstName)
+			assert.equal(responseAuthor.last_name, author.lastName)
+			assert.equal(responseAuthor.website_url, author.websiteUrl)
+			assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+			assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+			assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+			assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+			if (author.bios.length == 0) {
+				assert.isNull(responseAuthor.bio)
+			} else {
+				let authorBio = author.bios.find(b => b.language == language)
+
+				if (authorBio == null) {
+					assert.equal(responseAuthor.bio.language, "en")
+
+					authorBio = author.bios.find(b => b.language == "en")
+
+					assert.isNotNull(authorBio)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				} else {
+					assert.equal(responseAuthor.bio.language, language)
+					assert.equal(responseAuthor.bio.value, authorBio.bio)
+				}
+			}
+		}
+	})
+
+	it("should return latest authors with specified limit and language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listAuthorsEndpointUrl,
+				params: {
+					fields: "*",
+					languages: language,
+					limit: 1,
+					latest: true
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		// Find all authors with a profile image
+		let authors = [constants.authorUser.author]
+		for (let author of constants.davUser.authors) {
+			if (author.profileImageItem) authors.push(author)
+		}
+		authors = authors.reverse()
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, 1)
+
+		let responseAuthor = response.data.items[0]
+		let author = constants.davUser.authors.find(a => a.uuid == responseAuthor.uuid)
+
+		assert.isNotNull(author)
+		assert.equal(Object.keys(responseAuthor).length, 9)
+		assert.equal(responseAuthor.uuid, author.uuid)
+		assert.equal(responseAuthor.first_name, author.firstName)
+		assert.equal(responseAuthor.last_name, author.lastName)
+		assert.equal(responseAuthor.website_url, author.websiteUrl)
+		assert.equal(responseAuthor.facebook_username, author.facebookUsername)
+		assert.equal(responseAuthor.instagram_username, author.instagramUsername)
+		assert.equal(responseAuthor.twitter_username, author.twitterUsername)
+		assert.equal(responseAuthor.profile_image?.blurhash, author.profileImageItem?.blurhash)
+
+		if (author.bios.length == 0) {
+			assert.isNull(responseAuthor.bio)
+		} else {
+			let authorBio = author.bios.find(b => b.language == language)
+
+			if (authorBio == null) {
+				assert.equal(responseAuthor.bio.language, "en")
+
+				authorBio = author.bios.find(b => b.language == "en")
+
+				assert.isNotNull(authorBio)
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			} else {
+				assert.equal(responseAuthor.bio.language, language)
+				assert.equal(responseAuthor.bio.value, authorBio.bio)
+			}
+		}
+	})
+})
