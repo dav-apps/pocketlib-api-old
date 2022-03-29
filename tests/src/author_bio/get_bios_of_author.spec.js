8d2c82bbc248d6291b33a246a60134d4910e2fb3
David <dav2070@web.de>
2022-03-30 00:47:26 +0200

    Update get_bios_of_author to get bios of author of user and select fields
    - Add the ability to retrieve the bios of the author of the user with uuid = 'mine'
    - Add the ability to select the fields to include in the response body
    - Update tests

--- tests/src/author_bio/get_bios_of_author.spec.js
+++ tests/src/author_bio/get_bios_of_author.spec.js
@@ -4,10 +4,86 @@ import axios from 'axios'
 import constants from '../constants.js'
 import * as ErrorCodes from '../errorCodes.js'
 
-const getBiosOfAuthorEndpointUrl = `${constants.apiBaseUrl}/author/{0}/bios`
+const getBiosOfAuthorEndpointUrl = `${constants.apiBaseUrl}/authors/{0}/bios`
 
 describe("GetBiosOfAuthor endpoint", () => {
-	it("should not get bios of author that does not exist", async () => {
+	it("should not return bios of author of user with access token for session that does not exist", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', "mine"),
+				headers: {
+					Authorization: "asdasdasdasdasd"
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 404)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.SessionDoesNotExist)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return bios of author of user with access token for another app", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', "mine"),
+				headers: {
+					Authorization: constants.testUserTestAppAccessToken
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 403)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.ActionNotAllowed)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return bios of author of user if the user is an admin", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', "mine"),
+				headers: {
+					Authorization: constants.davUser.accessToken
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 400)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.UserIsAdmin)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return bios of author of user if the user is not an author", async () => {
+		try {
+			await axios({
+				method: 'get',
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', "mine"),
+				headers: {
+					Authorization: constants.testUser.accessToken
+				}
+			})
+		} catch (error) {
+			assert.equal(error.response.status, 400)
+			assert.equal(error.response.data.errors.length, 1)
+			assert.equal(error.response.data.errors[0].code, ErrorCodes.UserIsNotAuthor)
+			return
+		}
+
+		assert.fail()
+	})
+
+	it("should not return bios of author that does not exist", async () => {
 		try {
 			await axios({
 				method: 'get',
@@ -23,28 +99,66 @@ describe("GetBiosOfAuthor endpoint", () => {
 		assert.fail()
 	})
 
-	it("should get bios of author", async () => {
-		let author = constants.authorUser.author
+	it("should return bios of author", async () => {
+		let author = constants.davUser.authors[0]
 		let response
 
 		try {
 			response = await axios({
 				method: 'get',
-				url: getBiosOfAuthorEndpointUrl.replace('{0}', author.uuid)
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', author.uuid),
+				params: {
+					fields: "*"
+				}
 			})
 		} catch (error) {
 			assert.fail()
 		}
 
 		assert.equal(response.status, 200)
-		assert.equal(Object.keys(response.data.bios).length, author.bios.length)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(Object.keys(response.data.items).length, author.bios.length)
 
-		for (let key of Object.keys(response.data.bios)) {
-			let bio = author.bios.find(b => b.language == key)
+		for (let bio of author.bios) {
+			let responseBio = response.data.items.find(b => b.uuid == bio.uuid)
 
-			assert.isNotNull(bio)
-			assert.equal(key, bio.language)
-			assert.equal(response.data.bios[key], bio.bio)
+			assert.isNotNull(responseBio)
+			assert.equal(responseBio.uuid, bio.uuid)
+			assert.equal(responseBio.bio, bio.bio)
+			assert.equal(responseBio.language, bio.language)
+		}
+	})
+
+	it("should return bios of author of user", async () => {
+		let author = constants.authorUser.author
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: getBiosOfAuthorEndpointUrl.replace('{0}', "mine"),
+				headers: {
+					Authorization: constants.authorUser.accessToken
+				},
+				params: {
+					fields: "*"
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(Object.keys(response.data.items).length, author.bios.length)
+
+		for (let bio of author.bios) {
+			let responseBio = response.data.items.find(b => b.uuid == bio.uuid)
+
+			assert.isNotNull(responseBio)
+			assert.equal(responseBio.uuid, bio.uuid)
+			assert.equal(responseBio.bio, bio.bio)
+			assert.equal(responseBio.language, bio.language)
 		}
 	})
 })
