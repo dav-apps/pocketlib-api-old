3b7d03a833c2e9197a605bb0868b77f044010033
David <dav2070@web.de>
2022-04-07 22:29:58 +0200

    Update get_categories with standard response and ability to select response fields
    - Change the url path to /categories
    - Update tests
    - Remove unnecessary function for getting category table objects and add functions for getting names of category

--- tests/src/category/get_categories.spec.js
+++ tests/src/category/get_categories.spec.js
@@ -3,37 +3,84 @@ const assert = chai.assert
 import axios from 'axios'
 import constants from '../constants.js'
 
-const getCategoriesEndpointUrl = `${constants.apiBaseUrl}/store/categories`
+const getCategoriesEndpointUrl = `${constants.apiBaseUrl}/categories`
 
-describe("GetCategories endpoint", async () => {
+describe("GetCategories endpoint", () => {
 	it("should return categories", async () => {
 		let response
 
 		try {
 			response = await axios({
 				method: 'get',
-				url: getCategoriesEndpointUrl
+				url: getCategoriesEndpointUrl,
+				params: {
+					fields: "*"
+				}
 			})
 		} catch (error) {
 			assert.fail()
 		}
 
 		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, constants.categories.length)
 
-		let i = 0
 		for (let category of constants.categories) {
-			assert.equal(response.data.categories[i].uuid, category.uuid)
-			assert.equal(response.data.categories[i].key, category.key)
+			let responseCategory = response.data.items.find(c => c.uuid == category.uuid)
 
-			let j = 0
-			for (let categoryName of category.names) {
-				assert.equal(response.data.categories[i].names[j].name, categoryName.name)
-				assert.equal(response.data.categories[i].names[j].language, categoryName.language)
+			assert.isNotNull(responseCategory)
+			assert.equal(responseCategory.uuid, category.uuid)
+			assert.equal(responseCategory.key, category.key)
 
-				j++
+			let categoryName = category.names.find(n => n.language == "en")
+
+			assert.isNotNull(categoryName)
+			assert.equal(responseCategory.name.language, "en")
+			assert.equal(responseCategory.name.value, categoryName.name)
+		}
+	})
+
+	it("should return categories with specified language", async () => {
+		let language = "de"
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: getCategoriesEndpointUrl,
+				params: {
+					fields: "*",
+					languages: language
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, constants.categories.length)
+
+		for (let category of constants.categories) {
+			let responseCategory = response.data.items.find(c => c.uuid == category.uuid)
+
+			assert.isNotNull(responseCategory)
+			assert.equal(responseCategory.uuid, category.uuid)
+			assert.equal(responseCategory.key, category.key)
+
+			let categoryName = category.names.find(n => n.language == language)
+
+			if (categoryName == null) {
+				assert.equal(responseCategory.name.language, "en")
+
+				categoryName = category.names.find(n => n.language == "en")
+				
+				assert.isNotNull(categoryName)
+				assert.equal(responseCategory.name.value, categoryName.name)
+			} else {
+				assert.equal(responseCategory.name.language, language)
+				assert.equal(responseCategory.name.value, categoryName.name)
 			}
-
-			i++
 		}
 	})
 })
