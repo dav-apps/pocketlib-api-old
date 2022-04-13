d84f5b81e2e1098716f7f504b76df62b3a90d945
David <dav2070@web.de>
2022-04-13 20:21:48 +0200

    Add ability to list collections of series & update tests

--- tests/src/store_book_collection/list_store_book_collections.spec.js
+++ tests/src/store_book_collection/list_store_book_collections.spec.js
@@ -2,47 +2,31 @@ import chai from 'chai'
 const assert = chai.assert
 import axios from 'axios'
 import constants from '../constants.js'
-import * as ErrorCodes from '../errorCodes.js'
 
 const listStoreBookCollectionsEndpointUrl = `${constants.apiBaseUrl}/store_book_collections`
 
 describe("GetCollectionsOfAuthor endpoint", () => {
-	it("should not return collections of author that does not exist", async () => {
-		try {
-			await axios({
-				method: 'get',
-				url: listStoreBookCollectionsEndpointUrl,
-				params: {
-					author: "asdasdsda"
-				}
-			})
-		} catch (error) {
-			assert.equal(error.response.status, 404)
-			assert.equal(error.response.data.errors.length, 1)
-			assert.equal(error.response.data.errors[0].code, ErrorCodes.AuthorDoesNotExist)
-			return
-		}
-
-		assert.fail()
-	})
-
 	it("should return collections of author", async () => {
-		await testGetCollections(constants.authorUser.author)
+		await testGetCollectionsOfAuthor(constants.authorUser.author)
 	})
 
 	it("should return collections of author of admin", async () => {
-		await testGetCollections(constants.davUser.authors[0])
+		await testGetCollectionsOfAuthor(constants.davUser.authors[0])
 	})
 
 	it("should return collections of author with specified language", async () => {
-		await testGetCollectionsWithLanguage(constants.authorUser.author, "de")
+		await testGetCollectionsOfAuthorWithLanguage(constants.authorUser.author, "de")
 	})
 
 	it("should return collections of author of admin with specified language", async () => {
-		await testGetCollectionsWithLanguage(constants.davUser.authors[0], "de")
+		await testGetCollectionsOfAuthorWithLanguage(constants.davUser.authors[0], "de")
 	})
 
-	async function testGetCollections(author) {
+	it("should return collections of series", async () => {
+		await testGetCollectionsOfSeries(constants.authorUser.author, constants.authorUser.author.series[0])
+	})
+
+	async function testGetCollectionsOfAuthor(author) {
 		let response
 
 		try {
@@ -78,7 +62,7 @@ describe("GetCollectionsOfAuthor endpoint", () => {
 		}
 	}
 
-	async function testGetCollectionsWithLanguage(author, language) {
+	async function testGetCollectionsOfAuthorWithLanguage(author, language) {
 		let response
 
 		try {
@@ -120,6 +104,51 @@ describe("GetCollectionsOfAuthor endpoint", () => {
 				assert.equal(responseCollection.name.language, language)
 				assert.equal(responseCollection.name.value, collectionName.name)
 			}
+		}
+	}
+
+	async function testGetCollectionsOfSeries(author, series) {
+		let response
+
+		try {
+			response = await axios({
+				method: 'get',
+				url: listStoreBookCollectionsEndpointUrl,
+				params: {
+					fields: "*",
+					series: series.uuid
+				}
+			})
+		} catch (error) {
+			assert.fail()
+		}
+
+		let collections = []
+
+		for (let collectionUuid of series.collections) {
+			let collection = author.collections.find(c => c.uuid == collectionUuid)
+			assert.isNotNull(collection)
+
+			collections.push(collection)
+		}
+
+		assert.equal(response.status, 200)
+		assert.equal(Object.keys(response.data).length, 2)
+		assert.equal(response.data.items.length, collections.length)
+
+		for (let collection of collections) {
+			let responseCollection = response.data.items.find(c => c.uuid == collection.uuid)
+
+			assert.isNotNull(responseCollection)
+			assert.equal(Object.keys(responseCollection).length, 3)
+			assert.equal(responseCollection.uuid, collection.uuid)
+			assert.equal(responseCollection.author, author.uuid)
+
+			let collectionName = collection.names.find(n => n.language == "en")
+
+			assert.isNotNull(collectionName)
+			assert.equal(responseCollection.name.language, "en")
+			assert.equal(responseCollection.name.value, collectionName.name)
 		}
 	}
 })
