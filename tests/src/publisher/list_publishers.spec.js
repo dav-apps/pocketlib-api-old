e0addd91f552b3bdb76fbe03f479eec37d13b306
David <zurek.dav@gmail.com>
2023-06-07 22:45:13 +0200

    Update list publishers tests for changes in api

--- tests/src/publisher/list_publishers.spec.js
+++ tests/src/publisher/list_publishers.spec.js
@@ -10,7 +10,7 @@ describe("ListPublishers endpoint", () => {
 	it("should not return publishers of admin without access token", async () => {
 		try {
 			await axios({
-				method: 'get',
+				method: "get",
 				url: listPublishersEndpointUrl,
 				params: {
 					mine: true
@@ -19,7 +19,10 @@ describe("ListPublishers endpoint", () => {
 		} catch (error) {
 			assert.equal(error.response.status, 401)
 			assert.equal(error.response.data.errors.length, 1)
-			assert.equal(error.response.data.errors[0].code, ErrorCodes.AuthorizationHeaderMissing)
+			assert.equal(
+				error.response.data.errors[0],
+				ErrorCodes.AuthorizationHeaderMissing
+			)
 			return
 		}
 
@@ -29,7 +32,7 @@ describe("ListPublishers endpoint", () => {
 	it("should not return publishers of admin with access token for session that does not exist", async () => {
 		try {
 			await axios({
-				method: 'get',
+				method: "get",
 				url: listPublishersEndpointUrl,
 				headers: {
 					Authorization: "jksdfhjsdfjhksdf"
@@ -41,7 +44,10 @@ describe("ListPublishers endpoint", () => {
 		} catch (error) {
 			assert.equal(error.response.status, 404)
 			assert.equal(error.response.data.errors.length, 1)
-			assert.equal(error.response.data.errors[0].code, ErrorCodes.SessionDoesNotExist)
+			assert.equal(
+				error.response.data.errors[0],
+				ErrorCodes.SessionDoesNotExist
+			)
 			return
 		}
 
@@ -51,7 +57,7 @@ describe("ListPublishers endpoint", () => {
 	it("should not return publishers of admin with access token for another app", async () => {
 		try {
 			await axios({
-				method: 'get',
+				method: "get",
 				url: listPublishersEndpointUrl,
 				headers: {
 					Authorization: constants.testUserTestAppAccessToken
@@ -63,7 +69,10 @@ describe("ListPublishers endpoint", () => {
 		} catch (error) {
 			assert.equal(error.response.status, 403)
 			assert.equal(error.response.data.errors.length, 1)
-			assert.equal(error.response.data.errors[0].code, ErrorCodes.ActionNotAllowed)
+			assert.equal(
+				error.response.data.errors[0],
+				ErrorCodes.ActionNotAllowed
+			)
 			return
 		}
 
@@ -73,7 +82,7 @@ describe("ListPublishers endpoint", () => {
 	it("should not return publishers of admin if the user is not an admin", async () => {
 		try {
 			await axios({
-				method: 'get',
+				method: "get",
 				url: listPublishersEndpointUrl,
 				headers: {
 					Authorization: constants.authorUser.accessToken
@@ -85,7 +94,7 @@ describe("ListPublishers endpoint", () => {
 		} catch (error) {
 			assert.equal(error.response.status, 400)
 			assert.equal(error.response.data.errors.length, 1)
-			assert.equal(error.response.data.errors[0].code, ErrorCodes.UserIsNotAdmin)
+			assert.equal(error.response.data.errors[0], ErrorCodes.UserIsNotAdmin)
 			return
 		}
 
@@ -97,13 +106,14 @@ describe("ListPublishers endpoint", () => {
 
 		try {
 			response = await axios({
-				method: 'get',
+				method: "get",
 				url: listPublishersEndpointUrl,
 				headers: {
 					Authorization: constants.davUser.accessToken
 				},
 				params: {
-					fields: "*",
+					fields:
+						"uuid,name,description,website_url,facebook_username,instagram_username,twitter_username",
 					mine: true
 				}
 			})
@@ -112,23 +122,38 @@ describe("ListPublishers endpoint", () => {
 		}
 
 		assert.equal(response.status, 200)
-		assert.equal(Object.keys(response.data).length, 2)
-		assert.equal(response.data.items.length, constants.davUser.publishers.length)
+		assert.equal(
+			Object.keys(response.data.items).length,
+			constants.davUser.publishers.length
+		)
+		assert.equal(
+			response.data.items.length,
+			constants.davUser.publishers.length
+		)
 
 		for (let publisher of constants.davUser.publishers) {
-			let responsePublisher = response.data.items.find(p => p.uuid == publisher.uuid)
+			let responsePublisher = response.data.items.find(
+				p => p.uuid == publisher.uuid
+			)
 
 			assert.isNotNull(responsePublisher)
-			assert.equal(Object.keys(responsePublisher).length, 8)
+			assert.equal(Object.keys(responsePublisher).length, 7)
 			assert.equal(responsePublisher.uuid, publisher.uuid)
 			assert.equal(responsePublisher.name, publisher.name)
 			assert.equal(responsePublisher.description, publisher.description)
 			assert.equal(responsePublisher.website_url, publisher.websiteUrl)
-			assert.equal(responsePublisher.facebook_username, publisher.facebookUsername)
-			assert.equal(responsePublisher.instagram_username, publisher.instagramUsername)
-			assert.equal(responsePublisher.twitter_username, publisher.twitterUsername)
-			if (publisher.logoItem) assert.isNotNull(responsePublisher.logo?.url)
-			assert.equal(responsePublisher.logo?.blurhash, publisher.logoItem?.blurhash)
+			assert.equal(
+				responsePublisher.facebook_username,
+				publisher.facebookUsername
+			)
+			assert.equal(
+				responsePublisher.instagram_username,
+				publisher.instagramUsername
+			)
+			assert.equal(
+				responsePublisher.twitter_username,
+				publisher.twitterUsername
+			)
 		}
 	})
 })
